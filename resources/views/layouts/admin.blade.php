a4de740f85a8d20a3126b115dfb3dcac6db269da
pdac0926 <dancabilitazan@gmail.com>
2024-02-29 11:00:44 +0800

    Ui

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -141,7 +141,7 @@
                                 class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                                 <i class="icon rb-tasks-2-1 text-info text-sm opacity-10"></i>
                             </div>
-                            <span class="nav-link-text ms-1">Pending Appointment</span>
+                            <span class="nav-link-text ms-1">Pending Appointments</span>
                         </a>
                     </li>
                     <li class="nav-item">
